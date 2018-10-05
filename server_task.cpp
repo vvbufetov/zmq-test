3282d107931fa937b5ee7de8b2fb1063705fa4eb
Vyacheslav Bufetov <bw@rle.ru>
2018-10-05 17:33:19 +0300

    Subscription deletion is fixed. Some more debug output is added.

--- server_task.cpp
+++ server_task.cpp
@@ -4,14 +4,17 @@
 
 #include <thread>
 #include <iostream> // debug messages
+#include <vector>
 
+#define DEBUG_OUT
 #define CHECK_INTERVAL 1000
 
 void server_task::start () {
     std::vector<worker_task*> workers;
     try {
+#ifdef DEBUG_OUT
         std::cout << "[S] clients port: " << clients_port_ << "\n";
-
+#endif
         backend_.bind("inproc://workers");
         frontend_.bind(clients_port_);
         subscription_.bind("inproc://subscriptions");
@@ -25,14 +28,19 @@ void server_task::start () {
             worker_threads.emplace(worker_threads.end(), std::bind(&worker_task::start, *it));
             s_sleep(100);
         }
+#ifdef DEBUG_OUT
         std::cout << "[S] started\n";
-
+#endif
         zmq::pollitem_t items[] = {
             {frontend_, 0, ZMQ_POLLIN, 0},
             {backend_, 0, ZMQ_POLLIN, 0}
         };
 
         int64_t check_at = s_clock() + CHECK_INTERVAL;
+
+        // some debug stat
+        uint64_t routed_packets_cnt = 0;
+        uint64_t droped_packets_cnt = 0;
 
         while (!terminated_) {
             // 10 milliseconds
@@ -53,7 +61,9 @@ void server_task::start () {
                     case TSubscriptionCommand::UPDATE: {
                             auto it = timers_.find(client);
                             if (it == timers_.end()) {
+#ifdef DEBUG_OUT
                                 std::cerr << "[S] add subscription for " << client << "\n";
+#endif
                                 timers_[client] = {
                                     now + SUBSCRIBE_TIMEOUT,
                                     now + HEARTBEAT_TIMEOUT - CHECK_INTERVAL
@@ -66,13 +76,17 @@ void server_task::start () {
                                 frontend_.send( empty_msg );
                             }
                             else {
+#ifdef DEBUG_OUT
                                 std::cerr << "[S] update subscription for " << client << "\n";
+#endif
                                 it->second.expire_at = now + SUBSCRIBE_TIMEOUT;
                             }
                             break;
                         }
                     case TSubscriptionCommand::DELETE: {
+#ifdef DEBUG_OUT
                             std::cerr << "[S] drop subscription for " << client << "\n";
+#endif
                             timers_.erase(client);
                             break;
                         }
@@ -86,9 +100,11 @@ void server_task::start () {
                     subscription_.send( id_copy, ZMQ_SNDMORE );
                     cmd.send( subscription_ );
                 }
+#ifdef DEBUG_OUT
                 else {
                     std::cerr << "[S] invalid subscription: " << id_msg << "\n";
                 }
+#endif
             }
 
             // backend (from workers) //////////////////////////////////////////
@@ -117,16 +133,20 @@ void server_task::start () {
                                 // The empty timer record marks a dead subscription.
                                 // This item will be removed in expiration checking loop below.
                                 timers_[id] = {0, 0};
+                                droped_packets_cnt ++;
                             }
                             else
                             // Ignore dead subscribers.
                             if (it->second.expire_at != 0) {
+                                routed_packets_cnt ++;
                                 zmq::message_t data_copy;
                                 data_copy.copy( &data_msg );
                                 frontend_.send( id_copy, ZMQ_SNDMORE );
                                 frontend_.send( data_copy );
                                 it->second.heartbeat_at = now + HEARTBEAT_TIMEOUT - CHECK_INTERVAL;
                             }
+                            else
+                                droped_packets_cnt ++;
                         }
                     } while (id_msg.more());
                 }
@@ -135,17 +155,20 @@ void server_task::start () {
             // heartbeat round /////////////////////////////////////////////////
             if (now >= check_at) {
                 check_at = s_clock() + CHECK_INTERVAL;
+                std::vector<TClientID> expired_ids;
                 for (auto it = timers_.begin(); it != timers_.end(); ++it) {
                     if (it->second.expire_at <= now) {
+#ifdef DEBUG_OUT
                         std::cerr << "[S] ";
                         if (it->second.expire_at) std::cerr << "expire subscription for: ";
                         else std::cerr << "notify a dead subcription: ";
                         std::cerr << it->first << " at " << now << "\n";
-
+#endif
                         zmq::message_t id_msg(& it->first, sizeof(TClientID));
                         subscription_.send( id_msg, ZMQ_SNDMORE );
                         SubscriptionCommand cmd(TSubscriptionCommand::DELETE);
                         cmd.send( subscription_ );
+                        expired_ids.push_back( it->first );
                     }
                     else
                     if (it->second.heartbeat_at <= now) {
@@ -156,13 +179,32 @@ void server_task::start () {
                         frontend_.send( empty_msg );
                     }
                 }
+                if (expired_ids.size()) {
+#ifdef DEBUG_OUT
+                    std::cout << "[S] drop " << expired_ids.size() << " clients: ";
+                    for (auto it = expired_ids.begin(); it != expired_ids.end(); ++it ) {
+                        if (it != expired_ids.begin()) std::cout << ", ";
+                        std::cout << *it;
+                    }
+                    std::cout << std::endl;
+#endif
+                    for (auto id : expired_ids) timers_.erase( id );
+                }
+#ifdef DEBUG_OUT
+                std::cout << "[S] routed: " << routed_packets_cnt
+                    << " droped: " << droped_packets_cnt
+                    << " clients: " << timers_.size()
+                    << "\n";
+#endif
             }
         }
 
+#ifdef DEBUG_OUT
         std::cout << "[S] interrupted\n";
         for (auto ptr : workers) ptr->terminate();
         for (auto it=worker_threads.begin(); it != worker_threads.end(); ++it) it->join();
         std::cout << "[S] finished\n";
+#endif
     }
     catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
--- worker_task.cpp
+++ worker_task.cpp
@@ -2,17 +2,21 @@
 
 #define STAT_INTERVAL   3000
 
+//#define DEBUG_OUT
+
 void worker_task::start () {
     std::string header = "[W" + std::to_string(worker_index_) + "] ";
-
+#ifdef DEBUG_OUT
     std::cout << header << "sender port: " << sender_port_ << "\n";
+#endif
 
     backend_.connect(sender_port_);
     frontend_.connect("inproc://workers");
     subscription_.connect("inproc://subscriptions");
 
+#ifdef DEBUG_OUT
     std::cout << header << "started\n";
-
+#endif
     // some statistics for debugging
     uint64_t    ln_in = 0;
     uint64_t    ln_out = 0;
@@ -45,8 +49,10 @@ void worker_task::start () {
                     case TSubscriptionCommand::UPDATE: {
                             subs_upd_cnt ++;
                             auto it = filters_.find(client);
+#ifdef DEBUG_OUT
                             if (it == filters_.end())
                                 std::cout << header << "subscribe " << client << "\n";
+#endif
                             filters_[client] = cmd.filter;
                             break;
                         }
@@ -55,7 +61,9 @@ void worker_task::start () {
                             subs_del_cnt ++;
                             auto it = filters_.find(client);
                             if (it != filters_.end()) {
+#ifdef DEBUG_OUT
                                 std::cout << header << "UNsubscribe " << client << "\n";
+#endif
                                 filters_.erase(it);
                             }
                             break;
@@ -72,7 +80,6 @@ void worker_task::start () {
                 zmq::message_t  data_msg;
                 backend_.recv( &data_msg );
                 std::string line(static_cast<char *>(data_msg.data()), data_msg.size());
-                // std::cout << header << "line: " << line << "\n";
                 bool data_sent = false;
                 TClientID prev;
                 for (auto item : filters_) {
@@ -85,7 +92,6 @@ void worker_task::start () {
                             data_sent = true;
                             prev = item.first;
                         } else {
-                            //std::cout << header << " to " << prev << "\n";
                             zmq::message_t id_msg(&prev, sizeof(prev));
                             frontend_.send( id_msg, ZMQ_SNDMORE );
                             prev = item.first;
@@ -94,7 +100,6 @@ void worker_task::start () {
                 }
                 // sending the last item
                 if (data_sent) {
-                    //std::cout << header << " to " << prev << "\n";
                     zmq::message_t id_msg(&prev, sizeof(prev));
                     frontend_.send( id_msg ); // No SNDMORE flag
                 }
@@ -102,15 +107,19 @@ void worker_task::start () {
 
             if (now >= print_stat_at) {
                 print_stat_at = now + STAT_INTERVAL;
+#ifdef DEBUG_OUT
                 std::cout << header
                     << "in: " << ln_in
                     << " out: " << ln_out
                     << " upd: " << subs_upd_cnt
                     << " del: " << subs_del_cnt
                     << "\n";
+#endif
             }
         }
+#ifdef DEBUG_OUT
         std::cout << header << "interrupted\n";
+#endif
     }
     catch (std::exception &e) {
         std::cerr << header << e.what() << "\n";
@@ -118,10 +127,12 @@ void worker_task::start () {
     catch (...) {
         std::cerr << header << "exception\n";
     }
+#ifdef DEBUG_OUT
                 std::cout << header
                     << "in: " << ln_in
                     << " out: " << ln_out
                     << " upd: " << subs_upd_cnt
                     << " del: " << subs_del_cnt
                     << "\n";
+#endif
 }
